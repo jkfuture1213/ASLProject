1ac95bdf56e1d733b8b81b667002e9589315e453
jkfuture1213 <jkfuture1213@gmail.com>
2026-05-19 10:13:49 +0900

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace SmartAccountBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private System.Windows.Forms.DataGridView dgvEntries;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.ComboBox cbType;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.NumericUpDown nudAmount;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblType;
+        private System.Windows.Forms.Label lblCategory;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.Label lblAmount;
+
+        private void InitializeComponent()
+        {
+            this.dgvEntries = new System.Windows.Forms.DataGridView();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.cbType = new System.Windows.Forms.ComboBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.nudAmount = new System.Windows.Forms.NumericUpDown();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblType = new System.Windows.Forms.Label();
+            this.lblCategory = new System.Windows.Forms.Label();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.lblAmount = new System.Windows.Forms.Label();
+            this.btnResearch = new System.Windows.Forms.Button();
+            this.cbCategory = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEntries)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAmount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvEntries
+            // 
+            this.dgvEntries.AllowUserToAddRows = false;
+            this.dgvEntries.AllowUserToDeleteRows = false;
+            this.dgvEntries.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvEntries.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEntries.Location = new System.Drawing.Point(12, 124);
+            this.dgvEntries.Name = "dgvEntries";
+            this.dgvEntries.ReadOnly = true;
+            this.dgvEntries.RowHeadersWidth = 72;
+            this.dgvEntries.RowTemplate.Height = 23;
+            this.dgvEntries.Size = new System.Drawing.Size(804, 574);
+            this.dgvEntries.TabIndex = 0;
+            this.dgvEntries.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.dgvEntries_CellFormatting);
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Location = new System.Drawing.Point(68, 12);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(200, 32);
+            this.dtpDate.TabIndex = 1;
+            // 
+            // cbType
+            // 
+            this.cbType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbType.FormattingEnabled = true;
+            this.cbType.Items.AddRange(new object[] {
+            "수입",
+            "지출"});
+            this.cbType.Location = new System.Drawing.Point(356, 17);
+            this.cbType.Name = "cbType";
+            this.cbType.Size = new System.Drawing.Size(80, 29);
+            this.cbType.TabIndex = 2;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(78, 61);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(200, 32);
+            this.txtDescription.TabIndex = 4;
+            // 
+            // nudAmount
+            // 
+            this.nudAmount.Location = new System.Drawing.Point(356, 59);
+            this.nudAmount.Maximum = new decimal(new int[] {
+            1000000000,
+            0,
+            0,
+            0});
+            this.nudAmount.Name = "nudAmount";
+            this.nudAmount.Size = new System.Drawing.Size(100, 32);
+            this.nudAmount.TabIndex = 5;
+            this.nudAmount.ThousandsSeparator = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(489, 61);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 35);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "추가";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(570, 61);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(71, 35);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "삭제";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblTotal.Location = new System.Drawing.Point(12, 708);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(100, 30);
+            this.lblTotal.TabIndex = 8;
+            this.lblTotal.Text = "총합: 0원";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(12, 15);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(59, 21);
+            this.lblDate.TabIndex = 9;
+            this.lblDate.Text = "날짜:";
+            // 
+            // lblType
+            // 
+            this.lblType.AutoSize = true;
+            this.lblType.Location = new System.Drawing.Point(291, 20);
+            this.lblType.Name = "lblType";
+            this.lblType.Size = new System.Drawing.Size(59, 21);
+            this.lblType.TabIndex = 10;
+            this.lblType.Text = "구분:";
+            // 
+            // lblCategory
+            // 
+            this.lblCategory.AutoSize = true;
+            this.lblCategory.Location = new System.Drawing.Point(451, 20);
+            this.lblCategory.Name = "lblCategory";
+            this.lblCategory.Size = new System.Drawing.Size(101, 21);
+            this.lblCategory.TabIndex = 11;
+            this.lblCategory.Text = "카테고리:";
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(13, 61);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(59, 21);
+            this.lblDescription.TabIndex = 12;
+            this.lblDescription.Text = "메모:";
+            // 
+            // lblAmount
+            // 
+            this.lblAmount.AutoSize = true;
+            this.lblAmount.Location = new System.Drawing.Point(291, 64);
+            this.lblAmount.Name = "lblAmount";
+            this.lblAmount.Size = new System.Drawing.Size(59, 21);
+            this.lblAmount.TabIndex = 13;
+            this.lblAmount.Text = "금액:";
+            // 
+            // btnResearch
+            // 
+            this.btnResearch.Location = new System.Drawing.Point(130, 709);
+            this.btnResearch.Name = "btnResearch";
+            this.btnResearch.Size = new System.Drawing.Size(167, 32);
+            this.btnResearch.TabIndex = 14;
+            this.btnResearch.Text = "지출 내역 분석";
+            this.btnResearch.UseVisualStyleBackColor = true;
+            // 
+            // cbCategory
+            // 
+            this.cbCategory.FormattingEnabled = true;
+            this.cbCategory.Items.AddRange(new object[] {
+            "식비",
+            "교통",
+            "쇼핑",
+            "의료",
+            "문화",
+            "여행"});
+            this.cbCategory.Location = new System.Drawing.Point(559, 14);
+            this.cbCategory.Name = "cbCategory";
+            this.cbCategory.Size = new System.Drawing.Size(121, 29);
+            this.cbCategory.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 748);
+            this.Controls.Add(this.cbCategory);
+            this.Controls.Add(this.btnResearch);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.nudAmount);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.cbType);
+            this.Controls.Add(this.dtpDate);
+            this.Controls.Add(this.lblAmount);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.lblCategory);
+            this.Controls.Add(this.lblType);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.dgvEntries);
+            this.Name = "Form1";
+            this.Text = "가계부";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEntries)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAmount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnResearch;
+        private System.Windows.Forms.ComboBox cbCategory;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Data;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmartAccountBook
+{
+    public partial class Form1 : Form
+    {
+        private BindingList<Transaction> _transactions = new BindingList<Transaction>();
+        private DateTime _lastAdd = DateTime.MinValue;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            // setup DataGridView columns and binding
+            dgvEntries.AutoGenerateColumns = false;
+            dgvEntries.Columns.Clear();
+
+            var colDate = new DataGridViewTextBoxColumn { HeaderText = "날짜", DataPropertyName = "Date", Width = 90 };
+            var colType = new DataGridViewTextBoxColumn { HeaderText = "구분", DataPropertyName = "Type", Width = 60 };
+            var colCategory = new DataGridViewTextBoxColumn { HeaderText = "카테고리", DataPropertyName = "Category", Width = 100 };
+            var colDesc = new DataGridViewTextBoxColumn { HeaderText = "메모", DataPropertyName = "Description", AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill };
+            var colAmount = new DataGridViewTextBoxColumn { HeaderText = "금액", DataPropertyName = "Amount", Width = 120, DefaultCellStyle = { Alignment = DataGridViewContentAlignment.MiddleRight } };
+
+            dgvEntries.Columns.AddRange(new DataGridViewColumn[] { colDate, colType, colCategory, colDesc, colAmount });
+            dgvEntries.DataSource = _transactions;
+
+            _transactions.ListChanged += (s, e) => UpdateTotal();
+
+            try
+            {
+                LoadTransactionsFromJson();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("데이터 로드 오류: " + ex.Message);
+            }
+
+            UpdateTotal();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            var now = DateTime.Now;
+            if ((now - _lastAdd).TotalMilliseconds < 500) return;
+            _lastAdd = now;
+
+            btnAdd.Enabled = false;
+            try
+            {
+                DateTime date = dtpDate.Value.Date;
+                string type = cbType.SelectedItem as string ?? "지출";
+                string category = cbCategory.SelectedItem as string ?? "식비";
+                string desc = txtDescription.Text.Trim();
+                decimal amount = nudAmount.Value;
+
+                // ignore adding if amount is zero and no description/category
+                if (amount == 0 && string.IsNullOrEmpty(category) && string.IsNullOrEmpty(desc))
+                    return;
+
+                // 지출이면 금액을 음수로 저장하여 총합 계산에 반영
+                if (type == "지출") amount = -Math.Abs(amount);
+                else amount = Math.Abs(amount);
+
+                var t = new Transaction
+                {
+                    Date = date,
+                    Type = type,
+                    Category = category,
+                    Description = desc,
+                    Amount = amount
+                };
+
+                _transactions.Add(t);
+                try { SaveTransactionsToJson(); } catch { }
+
+                // 입력 필드 초기화
+                txtDescription.Clear();
+                nudAmount.Value = 0;
+            }
+            finally
+            {
+                btnAdd.Enabled = true;
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dgvEntries.SelectedRows.Count > 0)
+            {
+                var toRemove = dgvEntries.SelectedRows
+                    .Cast<DataGridViewRow>()
+                    .Select(r => r.DataBoundItem)
+                    .OfType<Transaction>()
+                    .ToList();
+
+                foreach (var t in toRemove)
+                {
+                    _transactions.Remove(t);
+                }
+                try { SaveTransactionsToJson(); } catch { }
+            }
+            else if (_transactions.Count > 0)
+            {
+                var last = _transactions[_transactions.Count - 1];
+                _transactions.RemoveAt(_transactions.Count - 1);
+                try { SaveTransactionsToJson(); } catch { }
+            }
+        }
+
+        private void UpdateTotal()
+        {
+            decimal total = _transactions.Sum(t => t.Amount);
+            lblTotal.Text = $"총합: {FormatWon(total)}";
+        }
+
+        private string JsonFilePath => Path.Combine(Application.StartupPath, "transactions.json");
+
+        private void LoadTransactionsFromJson()
+        {
+            _transactions.Clear();
+            if (!File.Exists(JsonFilePath)) return;
+            using (var fs = File.OpenRead(JsonFilePath))
+            {
+                var ser = new DataContractJsonSerializer(typeof(List<Transaction>));
+                var list = ser.ReadObject(fs) as List<Transaction>;
+                if (list != null)
+                {
+                    foreach (var t in list) _transactions.Add(t);
+                }
+            }
+        }
+
+        private void SaveTransactionsToJson()
+        {
+            var list = _transactions.ToList();
+            using (var fs = File.Create(JsonFilePath))
+            {
+                var ser = new DataContractJsonSerializer(typeof(List<Transaction>));
+                ser.WriteObject(fs, list);
+            }
+        }
+
+        private void dgvEntries_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            var dgv = sender as DataGridView;
+            if (dgv == null || e.ColumnIndex < 0) return;
+
+            var col = dgv.Columns[e.ColumnIndex];
+            if (col.DataPropertyName == "Amount" && e.Value != null)
+            {
+                if (e.Value is decimal dec) e.Value = FormatWon(dec);
+                else if (decimal.TryParse(e.Value.ToString(), out dec)) e.Value = FormatWon(dec);
+                e.FormattingApplied = true;
+            }
+
+            if (col.DataPropertyName == "Date" && e.Value != null)
+            {
+                if (e.Value is DateTime dt) e.Value = dt.ToString("yyyy-MM-dd");
+                e.FormattingApplied = true;
+            }
+        }
+
+        private string FormatWon(decimal value)
+        {
+            // 정수 단위로 처리, 음수 처리 포함
+            long v = (long)Math.Round(value, 0);
+            string abs = Math.Abs(v).ToString("N0");
+            return (v < 0 ? "-" : "") + abs + "원";
+        }
+
+        [DataContract]
+        private class Transaction
+        {
+            [DataMember]
+            public int Id { get; set; }
+            [DataMember]
+            public DateTime Date { get; set; }
+            [DataMember]
+            public string Type { get; set; }
+            [DataMember]
+            public string Category { get; set; }
+            [DataMember]
+            public string Description { get; set; }
+            [DataMember]
+            public decimal Amount { get; set; }
+        }
+    }
+}
