6e41e45637779983e001d80cbc9ec17da42d54b8
juhyeon030 <yuntn7@gamil.com>
2026-06-06 07:28:39 +0900

    로그인 5회 실패시 로그인 제한 걸리게 설정, 1번째 제한은 30분 제한, 두번째 제한은 1시간, 제한 걸릴 때 마다 30분 증가, 또한 로그인 몇회 실패했는지 카운트됨, 수정하다가 로그인이랑 회원가입 코드를 건드린건지 작동 안해서 확인 제대로 못했습니다.

---
+++ LoginLockForm.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace SmartAccountBook
+{
+    partial class LoginLockForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Label lblRemainTime;
+        private System.Windows.Forms.Timer timer1;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.lblRemainTime = new System.Windows.Forms.Label();
+
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+
+            this.SuspendLayout();
+
+            // lblTitle
+            this.lblTitle.Font = new System.Drawing.Font("맑은 고딕", 16F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.Location = new System.Drawing.Point(12, 20);
+            this.lblTitle.Size = new System.Drawing.Size(376, 40);
+            this.lblTitle.Text = "로그인 제한";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
+            // lblMessage
+            this.lblMessage.Font = new System.Drawing.Font("맑은 고딕", 10F);
+            this.lblMessage.Location = new System.Drawing.Point(12, 80);
+            this.lblMessage.Size = new System.Drawing.Size(376, 50);
+            this.lblMessage.Text = "로그인이 제한되었습니다.";
+            this.lblMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
+            // lblRemainTime
+            this.lblRemainTime.Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Bold);
+            this.lblRemainTime.ForeColor = System.Drawing.Color.Red;
+            this.lblRemainTime.Location = new System.Drawing.Point(12, 150);
+            this.lblRemainTime.Size = new System.Drawing.Size(376, 60);
+            this.lblRemainTime.Text = "00:00";
+            this.lblRemainTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
+            // timer1
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+
+            // LoginLockForm
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 250);
+
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lblRemainTime);
+
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.ControlBox = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "로그인 제한";
+
+            this.ResumeLayout(false);
+        }
+    }
+}
---
+++ LoginLockForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SmartAccountBook
+{
+    public partial class LoginLockForm : Form
+    {
+        private DateTime _lockUntil;
+
+        public LoginLockForm(DateTime lockUntil)
+        {
+            InitializeComponent();
+
+            _lockUntil = lockUntil;
+
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TimeSpan remain = _lockUntil - DateTime.Now;
+
+            if (remain.TotalSeconds <= 0)
+            {
+                timer1.Stop();
+
+                this.Close();
+
+                return;
+            }
+
+            lblRemainTime.Text =
+                $"{remain.Minutes:D2}:{remain.Seconds:D2}";
+        }
+    }
+}
